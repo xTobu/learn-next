a067de5930464dd6af01678d609203d9062fd623
xTobu <junxiang.oriint@gmail.com>
2018-01-10 12:23:12 +0800

    update

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <h1>Hello Next.js</h1>
+    </div>
+);
